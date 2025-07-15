cf6741017b520d8f3f4aeaca014d0a6866eee65b
鍾興偉 <zhongxingwei@MarkMacmini.local>
2025-07-15 11:27:02 +0800

    test conn db

---
+++ Northwind.Entities/NorthwindContext/Data/NorthwindContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Entities.NorthwindContext.Data
+{
+	public class NorthwindContext : DbContext
+    {
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options) { }
+    }
+}
+
